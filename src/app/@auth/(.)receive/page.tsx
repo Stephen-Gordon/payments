f025921d825cbdbd4c63270dee33a2a42cefe2d4
Ste <n00202054@iadt.ie>
2024-02-04 16:12:31 +0000

    starting receive page

---
+++ src/app/@auth/(.)receive/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+// React
+import { useState, useEffect } from 'react';
+import useGetAddress from '@/app/hooks/useGetAddress';
+// Next
+import Link from 'next/link';
+
+// Layout
+
+export default function Page() {
+
+
+  return (
+    <>
+      <div className='grid'>
+        <div className='my-4'>
+          <p className='my-4 text-center text-xl text-gray-300'>Receive</p>
+        </div>
+
+      </div>
+    </>
+  );
+}
---
+++ src/app/receive/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Page() {
+  return (
+    <>
+      <h1>Backup Receive Page</h1>
+    </>
+  );
+}
