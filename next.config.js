88ca5c28e0c4a288fc6360cf9c3d0fe99f8bb1f4
Stephen Gordon <n00202054@iadt.ie>
2024-02-12 11:43:58 +0000

    made pwa changes

--- next.config.js
+++ next.config.js
@@ -19,7 +19,6 @@ const withPWA = require('next-pwa')({
   dest: 'public', // Destination directory for the PWA files
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
-  display: 'standalone',
 });
 
 // Export the combined configuration for Next.js with PWA support
--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,5 +1,5 @@
 export const isProd = process.env.NODE_ENV === 'production';
-export const isLocal = process.env.NODE_ENV === 'development';
+export const isLocal = process.env.NODE_ENV === 'production';
 
 export const showLogger = isLocal
   ? true
