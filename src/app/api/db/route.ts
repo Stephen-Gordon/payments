3f637f3b9dc1c7d2d26d0841c62061b4fa60d4d9
Stephen Gordon <n00202054@iadt.ie>
2024-02-17 12:01:10 +0000

    fixed an import

--- src/app/api/db/route.ts
+++ src/app/api/db/route.ts
@@ -1,6 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { dummyDb } from '@/utils/db/in-memory-db'
-
+import { NextRequest, NextResponse } from 'next/server';
+import { dummyDb } from '@/app/utils/db/in-memory-db';
 export async function GET(_: NextRequest) {
-  return NextResponse.json(dummyDb)
+  return NextResponse.json(dummyDb);
 }
