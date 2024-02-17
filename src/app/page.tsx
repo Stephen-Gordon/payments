07546fafac477535e5810a820c64559372424253
Stephen Gordon <n00202054@iadt.ie>
2024-02-17 22:45:40 +0000

    removed roboto font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,10 +84,6 @@ export default function HomePage() {
           color='#5bbad5'
         />
         <link rel='shortcut icon' href='/favicon.ico' />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
-        />
 
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:url' content='https://yourdomain.com' />
