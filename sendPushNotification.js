98cb73368d3e052c68c4d411a57b980bfd7d7854
Stephen Gordon <n00202054@iadt.ie>
2024-02-21 11:26:24 +0000

    added keys

--- sendPushNotification.js
+++ sendPushNotification.js
@@ -2,18 +2,20 @@
 const webPush = require('web-push');
 
 const sendPushNotification = async () => {
-  const publicKey =
+  /* const publicKey =
     'BK9FZUL3u5bgvs8NlurUeFesIq5dm3qEUwOlh3hL7wGPbNec2SELGLwjKU2jWv9P9GULDvlWlC04Lric-w8yEf8';
-  const privateKey = 'EctJtRAxWnq18ayGbnjcHQ';
-
-  webPush.setVapidDetails(
+  const privateKey = 'EctJtRAxWnq18ayGbnjcHQ'; */
+  const publicKey =
+    'BKIvDJTEdSWEOc3P_-QtcaNhcBYbpESr6KXM2S7oCmlnkdgwAz1wHn8T17OZDrpkDw5GkfiHwrePpgzh55e4Qt4';
+  const privateKey = '5Lys2yFtv71OQnusHfZNNQ';
+  /* webPush.setVapidDetails(
     'mailto:example@yourdomain.org',
-    'BKygzgxdX4Y-kic62cOFL6SYlEjIXryAyd6i1lqvp2N9F8UQ9-eSCUpyoQxryU-Q-E1Xn0AxdFOZFvFxdVksKdI',
-    'AWDM9R15IL7prFSo3EmNjIyoz3w8f1d5-bbp01emJHo'
-  );
+    publicKey,
+    privateKey
+  ); */
   const pushSubscription = {
     endpoint:
-      'https://fcm.googleapis.com/fcm/send/cXPpv3DCUkc:APA91bG_RXITNq0m0-qPV6Uf6BRyrk31c7J_RIh-ucDkQxhLa6twipViS1WI_p6uR6FBuTy_HHGR2S9g7a8TTF35cEwO1QXb8zUXwxyb7D3ZQESVE8BLKjR63DqSeyBcgcuxuZZBm6ND',
+      'https://fcm.googleapis.com/fcm/send/cFpbxV7KLYc:APA91bHFlDQI1HYpYr7NMl2Y67B6WtjQRcejgewG1jhRPeJWR3wrIgIrYpWgkv8tOMsPUmAJcFHsB3up8EvpVWjfntCnir34fE827JK8u56fQ2FSmE8dhWZva21_lu08SxO2UUXjtSLf',
     expirationTime: Math.floor(Date.now() / 1000) + 10,
 
     keys: {
@@ -30,7 +32,7 @@ const sendPushNotification = async () => {
   ); */
   console.log(pushSubscription);
   try {
-    await fetch('http://localhost:3000/notification', {
+    await fetch('https://payments-lyart.vercel.app/notifications', {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
