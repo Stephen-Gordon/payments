3dda8db89a11e9154c9fdb49a7159bdaecdd4e00
Stephen Gordon <n00202054@iadt.ie>
2024-03-08 15:47:29 +0000

    wored on viewport

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
+     {/*  <Head>
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1, maximum-scale=1'
@@ -11,7 +11,7 @@ export default function Document() {
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/icon.png'></link>
         <meta name='theme-color' content='#2b2b2b' />
-      </Head>
+      </Head> */}
       <body>
         <Main />
         <NextScript />
