7107ada5fe7a01c16ef3f4ad58c9e9652373c226
Stephen Gordon <n00202054@iadt.ie>
2024-03-24 18:31:53 +0000

    changed the succes ui

--- src/app/components/Success/Success.tsx
+++ src/app/components/Success/Success.tsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { CheckCircle2, Loader2 } from 'lucide-react';
 
 import { RotatingLines } from 'react-loader-spinner';
-import { Drawer, DrawerContent } from '../ui/drawer';
+import { Drawer, DrawerContent, DrawerOverlay } from '../ui/drawer';
 import { useRouter } from 'next/navigation';
 
 // redux
@@ -16,6 +16,38 @@ import { setTransactions } from '@/GlobalRedux/Features/transactions/transaction
 // hooks
 import useGetRecentTransactions from '@/app/hooks/useGetRecentTransactions';
 import useGetAddress from '@/app/hooks/useGetAddress';
+
+import { MultiStepLoader as Loader } from '../ui/multi-step-loader';
+import { IconSquareRoundedX } from '@tabler/icons-react';
+import { TextGenerateEffect } from '../ui/text-generate-effect';
+
+const loadingStates = [
+  {
+    text: 'Loading your Payment',
+  },
+  {
+    text: "It's on the way",
+  },
+  {
+    text: 'Meeting Tyler Durden',
+  },
+  {
+    text: 'He makes soap',
+  },
+  {
+    text: 'We goto a bar',
+  },
+  {
+    text: 'Start a fight',
+  },
+  {
+    text: 'We like it',
+  },
+  {
+    text: 'Welcome to F**** C***',
+  },
+];
+
 
 export default function Success({
   transactionStatus,
@@ -54,33 +86,75 @@ export default function Success({
     }
   }, [transactionStatus]);
   return (
-    <Drawer nested={true} dismissible={false} open={true}>
-      <DrawerContent
-        style={{ borderRadius: '40px', padding: '16px' }}
-        className='bg-background inset fixed z-50 grid h-fit space-y-8 overflow-hidden rounded-t-[40px] border-t bg-opacity-100 '
-      >
-        <div className='relative w-full items-center justify-center'>
-          {transactionStatus && (
-            <div key={'check-circle'} className='grid w-full justify-center '>
-              <CheckCircle2 size='64' color='#4ade80' />
-            </div>
-          )}
-          {loading && (
-            <div key={'loader'} className='grid w-full justify-center '>
-              <div className='grid w-full justify-center '>
-                <Loader2
-                  size='64'
-                  className='animate-spin justify-self-center '
-                  color='white'
-                />
+    <>
+      <Drawer nested={true} dismissible={false} open={true}>
+        <DrawerContent
+          dontShowDrag={true}
+          className=' flex h-full w-full border-none bg-black/90 transition-all duration-400 ease-in-out'
+        >
+          {/*    <motion.div
+            className='ios'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: loading ? 0.7 : 0 }}
+            transition={{ duration: 0.3 }}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100vw',
+              height: '100vh',
+              backgroundColor: 'rgba(0, 0, 0, 0.7)',
+              zIndex: '-50', // adjust the z-index as needed
+            }}
+          /> */}
+          <motion.div
+            key='success'
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
+            exit={{ opacity: 0, y: 20 }}
+            className='fixed bottom-2 mb-auto w-full pb-8 shadow-lg px-2'
+          >
+            <div className=' text-card-foreground  h-full w-full rounded-2xl  border shadow'>
+              <div
+                style={{ paddingTop: '16px', paddingBottom: '16px' }}
+                className='relative w-full items-center justify-center space-y-8 py-8'
+              >
+                {transactionStatus && (
+                  <div
+                    key={'check-circle'}
+                    className='grid w-full justify-center '
+                  >
+                    <CheckCircle2 size='64' color='#4ade80' />
+                  </div>
+                )}
+                {loading && (
+                  <div key={'loader'} className='grid w-full justify-center '>
+                    <div className='grid w-full justify-center '>
+                      <Loader2
+                        size='64'
+                        className='stroke-muted-foreground animate-spin justify-self-center '
+                      />
+                    </div>
+
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ delay: 0.4 }}
+                      className='w-full'
+                    >
+                      <TextGenerateEffect
+                        className='mt-4 text-center text-sm text-black'
+                        words='Your payment is on the way'
+                      ></TextGenerateEffect>
+                    </motion.div>
+                  </div>
+                )}
               </div>
-              <p className='text-muted-foreground text-center text-sm'>
-                Your payment is on the way
-              </p>
             </div>
-          )}
-        </div>
-      </DrawerContent>
-    </Drawer>
+          </motion.div>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
