19a715313ecc3aab553ced7fc943cd6b1e2919b1
Stephen Gordon <n00202054@iadt.ie>
2024-03-24 15:24:31 +0000

    changed icon colors

--- src/app/components/BottomNav/BottomNav.tsx
+++ src/app/components/BottomNav/BottomNav.tsx
@@ -32,6 +32,8 @@ const BottomNavbar = () => {
         <nav className='bg-background/50 fixed bottom-0 z-40 flex w-full justify-between overflow-hidden bg-opacity-10 bg-clip-padding p-4 px-8 pb-8 saturate-200 backdrop-blur-xl backdrop-filter'>
           <Link href='/home'>
             <Home
+              className='stroke-muted-foreground'
+              strokeWidth={1}
               onClick={() => {
                 dispatch(setSheet(false));
               }}
@@ -39,6 +41,8 @@ const BottomNavbar = () => {
           </Link>
           <Link href='/search'>
             <Send
+              className='stroke-muted-foreground'
+              strokeWidth={1}
               onClick={() => {
                 dispatch(setSheet(true));
               }}
@@ -52,7 +56,8 @@ const BottomNavbar = () => {
               dispatch(setSheet(true));
             }}
           >
-            <BookUser />
+            <BookUser
+              className='stroke-muted-foreground' strokeWidth={1} />
           </Link>
           <Link
             href={{
@@ -60,7 +65,11 @@ const BottomNavbar = () => {
               query: { isNavOpen: true },
             }}
           >
-            <Settings onClick={() => router.push(`/menu?isNavOpen=true`)} />
+            <Settings
+              className='stroke-muted-foreground'
+              strokeWidth={1}
+              onClick={() => router.push(`/menu?isNavOpen=true`)}
+            />
           </Link>
         </nav>
       )}
