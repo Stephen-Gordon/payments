9901e42485b73bec80038c4e7821c0962159e029
Stephen Gordon <n00202054@iadt.ie>
2024-03-26 12:01:51 +0000

    fixed filtering issue

--- src/app/hooks/useFindPayeeName.tsx
+++ src/app/hooks/useFindPayeeName.tsx
@@ -4,16 +4,19 @@ import { Contact } from '@/app/types/types';
 import truncateEthAddress from 'truncate-eth-address';
 
 const useFindPayeeName = (payeeAddress: string) => {
+
   const contactsState = useSelector((state: RootState) => state.contacts.value);
 
   const findPayeeName = (payeeAddress: string): string | null => {
-    if ( contactsState.length === 0) {
+    if ( contactsState.length == 0) {
       return truncateEthAddress(payeeAddress);
     }
 
+    // make sure theyre lowercase to match them
     const contact = contactsState.find(
-      (contact: Contact) => contact.address === payeeAddress
+      (element: Contact) => element.address?.toLocaleLowerCase() == payeeAddress.toLocaleLowerCase()
     );
+    console.log("contact ", contact)
 
     return contact ? contact.name : truncateEthAddress(payeeAddress);
   };
