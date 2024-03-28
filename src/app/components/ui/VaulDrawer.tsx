45cdb0825dd85603cb94cfc02228589ad1a31706
Stephen Gordon <n00202054@iadt.ie>
2024-03-28 17:11:38 +0000

    changed bg color

--- src/app/components/ui/VaulDrawer.tsx
+++ src/app/components/ui/VaulDrawer.tsx
@@ -20,7 +20,7 @@ export function VaulDrawer({children}) {
       shouldScaleBackground
     >
       <Drawer.Portal>
-        <Drawer.Overlay className='fixed inset-0 bg-black/40' />
+        <Drawer.Overlay className='bg-background/40 fixed inset-0' />
         <Drawer.Content className='from-background to bg-background/80 fixed bottom-0 left-0 right-0 mt-24 flex h-[96%] flex-col rounded-t-[10px] bg-gradient-to-t backdrop-blur-2xl'>
           {children}
         </Drawer.Content>
