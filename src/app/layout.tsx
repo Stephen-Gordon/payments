9a5344657d9c98ada6638eef5d6135471cf536ac
Stephen Gordon <n00202054@iadt.ie>
2024-03-31 17:19:05 +0100

    added icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,41 +109,10 @@ export default function RootLayout({
         <meta name='msapplication-TileColor' content='#020817' />
         <meta name='msapplication-tap-highlight' content='no' />
         <meta name='theme-color' content='#020817' />
-        <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-        <link
-          rel='apple-touch-icon'
-          sizes='152x152'
-          href='/icons/touch-icon-ipad.png'
-        />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/icons/touch-icon-iphone-retina.png'
-        />
-        <link
-          rel='apple-touch-icon'
-          sizes='167x167'
-          href='/icons/touch-icon-ipad-retina.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/icons/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/icons/favicon-16x16.png'
-        />
+
+      
         <link rel='manifest' href='/manifest.json' />
-        <link
-          rel='mask-icon'
-          href='/icons/safari-pinned-tab.svg'
-          color='#020817'
-        />
-        <link rel='shortcut icon' href='/favicon.ico' />
+        
         <link rel='preconnect' href='https://fonts.googleapis.com'></link>
         <link rel='preconnect' href='https://fonts.gstatic.com'></link>
         <link
