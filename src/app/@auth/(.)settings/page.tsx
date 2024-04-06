e518432b69f760cc1db5b4f3722fcb7c02885428
Stephen Gordon <n00202054@iadt.ie>
2024-04-06 23:17:47 +0100

    Added logout button and clear cache button

--- src/app/@auth/(.)settings/page.tsx
+++ src/app/@auth/(.)settings/page.tsx
@@ -9,17 +9,53 @@ import {
   DrawerTitle,
 } from '@/app/components/ui/drawer';
 import { Button } from '@/app/components/ui/button';
+
 import SendNotification from '@/app/components/SendNotification/SendNotification';
+
+import { useLogout } from '@privy-io/react-auth';
 
 // next
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { usePrivySmartAccount } from '@zerodev/privy';
 
 export default function Page() {
 
+ // const { logout } = usePrivySmartAccount()
+
+ const router = useRouter();
+
+
+  const { logout } = useLogout({
+    onSuccess: () => {
+      console.log('User logged out');
+      router.push('/login');
+      localStorage.clear();
+      // Any logic you'd like to execute after a user successfully logs out
+    },
+  });
+
   return (
     <>
-      <SendNotification />
+      <div className='grid space-y-6 p-4'>
+        <SendNotification />
+        <Button
+          variant={'destructive'}
+          onClick={() => {
+            logout();
+          }}
+        >
+          Log out
+        </Button>
+        <Button
+          variant={'destructive'}
+          onClick={() => {
+            localStorage.clear();
+          }}
+        >
+          Clear Local Storage
+        </Button>
+      </div>
     </>
   );
 }
