27f5bacc0eec633f683f83bfe8dd5ae1d7e8df20
Ste <n00202054@iadt.ie>
2024-04-07 13:55:00 +0100

    testing ios translucent tool bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta
           name='apple-mobile-web-app-status-bar-style'
-          content='black-transparent'
+          content='black-translucent'
         />
         <meta name='mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-title' content='Payments' />
