b5905e91e9c50422f63bb30f9835f5fe7c5245e5
Stephen Gordon <n00202054@iadt.ie>
2024-04-10 21:25:15 +0100

    added animate presence

--- src/app/@auth/layout.tsx
+++ src/app/@auth/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 import * as React from 'react';
 import AuthPage from '../components/AuthPage/AuthPage';
 import { VaulDrawer } from '../components/ui/VaulDrawer';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function ComponentsLayout({
   children,
@@ -15,15 +15,17 @@ export default function ComponentsLayout({
     <>
       <AuthPage>
         <VaulDrawer>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5, ease: 'easeInOut' }}
-            className='overflow-y-scroll'
-          >
-            {children}
-          </motion.div>
+          <AnimatePresence mode='wait'>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+              className='overflow-y-scroll'
+            >
+              {children}
+            </motion.div>
+          </AnimatePresence>
         </VaulDrawer>
       </AuthPage>
     </>
