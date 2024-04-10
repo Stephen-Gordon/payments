d8e58f8a9f89f24f9909996e344ebdeae65eb004
Stephen Gordon <n00202054@iadt.ie>
2024-04-10 14:08:44 +0100

    added animation complete callback function as a prop

--- src/app/components/ui/text-generate-effect.tsx
+++ src/app/components/ui/text-generate-effect.tsx
@@ -2,15 +2,21 @@
 import { useEffect } from 'react';
 import { motion, stagger, useAnimate } from 'framer-motion';
 import { cn } from '@/lib/utils';
+
 export const TextGenerateEffect = ({
   words,
   className,
+  onAnimationComplete,
+  delay
 }: {
   words: string;
   className?: string;
+  onAnimationComplete?: () => void;
+  delay?: number;
 }) => {
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(' ');
+
   useEffect(() => {
     animate(
       'span',
@@ -19,9 +25,13 @@ export const TextGenerateEffect = ({
       },
       {
         duration: 1,
-        delay: stagger(0.1),
+        delay: stagger(delay || 0.1),
       }
-    );
+    ).then(() => {
+      if (onAnimationComplete) {
+        onAnimationComplete();
+      }
+    });
   }, [scope.current]);
 
   const renderWords = () => {
@@ -29,11 +39,8 @@ export const TextGenerateEffect = ({
       <motion.div ref={scope}>
         {wordsArray.map((word, idx) => {
           return (
-            <motion.span
-              key={word + idx}
-              className='opacity-0 '
-            >
-              {word}{' '}
+            <motion.span key={word + idx} className='opacity-0 '>
+              {word === '\\n' ? <br /> : word}{' '}
             </motion.span>
           );
         })}
@@ -42,10 +49,8 @@ export const TextGenerateEffect = ({
   };
 
   return (
-    
-      <div className=' font-normal leading-relaxed tracking-wide text-inherit  '>
-        {renderWords()}
-      </div>
-
+    <div className='font-normal leading-relaxed tracking-wide text-inherit'>
+      {renderWords()}
+    </div>
   );
 };
