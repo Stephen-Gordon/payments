6b7104c7f0dd7329837eeb205c14de9923561712
Stephen Gordon <n00202054@iadt.ie>
2024-04-11 10:12:18 +0100

    removed unused folders

--- src/app/api/moralis/[...moralis].ts
+++
@@ -1,6 +0,0 @@
-import { MoralisNextApi } from "@moralisweb3/next";
-
-export default MoralisNextApi({
-  apiKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjY4NTRjOTU5LTk5NGEtNGYwZC04MjA0LTA4YWM2MmE3Y2EwOSIsIm9yZ0lkIjoiMzc2ODcxIiwidXNlcklkIjoiMzg3Mjg5IiwidHlwZUlkIjoiMTQxNjQyOTUtNzg4MS00Yzk2LTkxM2ItNTQ5YjBiZjU2M2FkIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDc3NDAwOTgsImV4cCI6NDg2MzUwMDA5OH0.GrOEC01ujomn_3cVNZKKxCkd5QVP52i5I3HsFxSp23Q',
-});
