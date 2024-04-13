a738c139220c5de44b82db3ea1d49024fb5c2b9d
Stephen Gordon <n00202054@iadt.ie>
2024-04-13 11:33:33 +0100

    added delay

--- src/app/components/ui/VaulDrawer.tsx
+++ src/app/components/ui/VaulDrawer.tsx
@@ -4,15 +4,33 @@ import { Drawer } from 'vaul';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSheet } from '@/GlobalRedux/Features/sheet/sheetSlice';
 
+import { useEffect, useState, useRef } from 'react';
+
+import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+
 export function VaulDrawer({children}) {
 
       const isOpen = useSelector((state) => state.sheet.value);
       const dispatch = useDispatch();
 
+      const pathname = usePathname();
+
+      const [pathnameState, setPathnameState] = useState('');
+        const prevCountRef = useRef<number>('');
+
+
+      useEffect(() => {
+        console.log('pathname', pathname);
+        setPathnameState(pathname);
+      }, [pathname]);
+
+      
+
+
 
   return (
     <Drawer.Root
-    
       onClose={() => {
         dispatch(setSheet(false));
         window.history.pushState({}, '', '/home');
@@ -21,9 +39,23 @@ export function VaulDrawer({children}) {
       shouldScaleBackground
     >
       <Drawer.Portal>
-        <Drawer.Overlay className='bg-background/40 fixed inset-0' />
-        <Drawer.Content className='z-50 from-background to bg-background/80 fixed bottom-0 left-0 right-0 mt-24 flex h-[96%] flex-col rounded-t-[10px] bg-gradient-to-t backdrop-blur-2xl'>
-          {children}
+        <Drawer.Overlay
+          onClick={() => dispatch(setSheet(false))}
+          className='bg-background/40 fixed inset-0'
+        />
+        <Drawer.Content className='from-background to bg-background/80 fixed bottom-0 left-0 right-0 z-50 mt-24 flex h-[96%] flex-col rounded-t-[10px] bg-gradient-to-t backdrop-blur-2xl'>
+          {isOpen && (
+            <>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.2 }}
+              >
+                {children}
+              </motion.div>
+            </>
+          )}
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
